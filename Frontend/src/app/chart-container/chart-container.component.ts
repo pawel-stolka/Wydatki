ed462cfcd1ecf97dc1062395f882f8e6259e4c29
Pablo <pawel.stolka@gmail.com>
2018-06-24 10:41:22 +0200

    update each 1000ms

--- Frontend/src/app/chart-container/chart-container.component.ts
+++ Frontend/src/app/chart-container/chart-container.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart-container.component.css']
 })
 export class ChartContainerComponent implements OnInit {
-
+  private chartData: Array<any>;
+  
   constructor() { }
-
+  
   ngOnInit() {
+    this.generateData()
+
+     // change the data periodically
+    setInterval(() => {
+      this.generateData(), 100
+    }, 1000);
+  }
+
+  generateData(): any {
+    this.chartData = [];
+
+    // // let's say 5 bars
+    // for (var i = 1; i <= 5; i++) {
+    //   this.chartData.push([
+    //     `${i}.`,
+    //     Math.floor(Math.random() * 10)
+    //   ])
+    // }
+
+    // random interval
+    let rowNumber = 5 + Math.floor(Math.random() * 10);
+    for (var i = 1; i <= rowNumber; i++) {
+      this.chartData.push([
+        `${i}.`,
+        Math.floor(Math.random() * 10)
+      ])
+    }
+    console.log(this.chartData)
   }
 
 }
--- Frontend/src/app/chart-update/chart-update.component.ts
+++ Frontend/src/app/chart-update/chart-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'chart-update',
@@ -8,10 +9,85 @@ import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
 export class ChartUpdateComponent implements OnInit {
   @ViewChild('chart') private chartContainer: ElementRef;
   @Input() private data: Array<any>;
+
+  private chart: any;
+  private margin: any = 25;
+  private width;
+  private height;
+
+  private xScale
+  private yScale
+  private colors
+  private xAxis
+  private yAxis
   
   constructor() { }
 
   ngOnInit() {
+    this.createChart();
+    if (this.data) {
+      this.updateChart();
+    }
+  }
+
+  ngOnChanges() {
+    if (this.chart) {
+      this.updateChart();
+    }
+  }
+
+  createChart() {
+    let element = this.chartContainer.nativeElement;
+    this.width = element.offsetWidth - 2 *this.margin;
+    this.height = element.offsetHeight - 2 * this.margin;
+    let svg = d3.select(element).append('svg')
+      .attr('width', element.offsetWidth)
+      .attr('height', element.offsetHeight);
+
+    // chart plot area
+    this.chart = svg.append('g')
+      .attr('class', 'bars')
+      .attr('transform', `translate(${this.margin}, ${this.margin})`);
+
+    // define X & Y domains
+    let xDomain = this.data.map(d => d[0]);
+    let yDomain = [0, d3.max(this.data, d => d[1])];
+
+    // create scales
+    this.xScale = d3.scaleBand().padding(0.1)
+      .domain(xDomain)
+      .rangeRound([0, this.width]);
+
+    this.yScale = d3.scaleLinear()
+      .domain(yDomain)
+      .range([this.height, 0]);
+
+    // bar colors
+    this.colors = d3.scaleLinear()
+      .domain([0, this.data.length])
+      .range(<any[]>['red', 'blue']);
+
+    // x & y axis
+    this.xAxis = svg.append('g')
+      .attr('class', 'axis axis-x')
+      .attr('transform', `translate(${this.margin}, ${this.margin + this.height})`)
+      .call(d3.axisBottom(this.xScale));
+    this.yAxis = svg.append('g')
+      .attr('class', 'axis axis-y')
+      .attr('transform', `translate(${this.margin}, ${this.margin})`)
+      .call(d3.axisLeft(this.yScale));
+  }
+
+  updateChart() {
+     // update scales & axis
+     this.xScale.domain(this.data.map(d => d[0]));
+     this.yScale.domain([0, d3.max(this.data, d => d[1])]);
+     this.colors.domain([0, this.data.length]);
+     this.xAxis.transition().call(d3.axisBottom(this.xScale));
+     this.yAxis.transition().call(d3.axisLeft(this.yScale));
+
+     let update = this.chart.selectAll('.bar')
+      .data(this.data);
   }
 
 }
