fb08e4590f8ddd1b49030815829ec37efe651d43
Pablo <pawel.stolka@gmail.com>
2018-06-24 10:49:10 +0200

    update bars

--- Frontend/src/app/chart-update/chart-update.component.ts
+++ Frontend/src/app/chart-update/chart-update.component.ts
@@ -88,6 +88,32 @@ export class ChartUpdateComponent implements OnInit {
 
      let update = this.chart.selectAll('.bar')
       .data(this.data);
+
+    // remove existing bars
+    update.exit().remove();
+    
+    // update existing bars
+    this.chart.selectAll('.bar').transition()
+      .attr('x', d => this.xScale(d[0]))
+      .attr('y', d => this.yScale(d[1]))
+      .attr('width', d => this.xScale.bandwidth())
+      .attr('height', d => this.height - this.yScale(d[1]))
+      .style('fill', (d, i) => this.colors(i));
+
+    // add new bars
+    update
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => this.xScale(d[0]))
+      .attr('y', d => this.yScale(0))
+      .attr('width', this.xScale.bandwidth())
+      .attr('height', 0)
+      .style('fill', (d, i) => this.colors(i))
+      .transition()
+      .delay((d, i) => i * 10)
+      .attr('y', d => this.yScale(d[1]))
+      .attr('height', d => this.height - this.yScale(d[1]));
   }
 
 }
