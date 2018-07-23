f7ce2a469822b5b6b5ccab5f0a47fd2c6fee34db
Pablo <pawel.stolka@gmail.com>
2018-07-24 00:24:42 +0200

    ready ??

---
+++ API/connection.js
@@ -0,0 +1,4 @@
+module.exports.db = {
+    url: //'mongodb://localhost:27017/Wydatki'
+        'mongodb://user12:pass12@ds161520.mlab.com:61520/wydatki10'
+}
--- API/server.js
+++ API/server.js
@@ -3,14 +3,14 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
-    Bill = require('./models/Bill')
+    Bill = require('./models/Bill'),
+    connection = require('./connection')
 
 mongoose.Promise = Promise
 
 var app = express(),
     port = process.env.PORT || 7000,
-    mongoString = //'mongodb://localhost:27017/Wydatki'
-    'mongodb://user12:pass12@ds161520.mlab.com:61520/wydatki10'
+    mongoString = connection.db.url
 
 app.use(cors())
 app.use(bodyParser.json())
