173560976cb1ed05f162e9a6c73886adc013bb78
Pablo <pawel.stolka@gmail.com>
2018-07-24 08:31:29 +0200

    begin1 user

--- API/connection.js
+++ API/connection.js
@@ -1,3 +1,4 @@
 module.exports.db = {
-    url: 'here goes your own mLab connection string'
+    // url: 'here goes your own mLab connection string'
+    url: 'mongodb://begin1:begin1@ds161520.mlab.com:61520/wydatki10'
 }
